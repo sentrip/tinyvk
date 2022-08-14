bf618a99216fb19808d6c6e3074b74bf775b09c1
Djordje Pepic <djordje.m.pepic@gmail.com>
2022-08-14 16:18:47 +0200

    Fixed bug in bitset

--- src/tinystd_bitset.h
+++ src/tinystd_bitset.h
@@ -13,9 +13,9 @@ template<size_t N>
 struct bitset {
     uint64_t words[1 + ((N - 1) / 64)]{};
 
-    [[nodiscard]] constexpr bool test  (size_t i)          const noexcept { return (words[i / N] & (1ull << (i % N))) > 0; }
-                  constexpr void set   (size_t i, bool v = true) noexcept { words[i / N] |= (uint64_t(v) << (i % N)); }
-                  constexpr void reset (size_t i)                noexcept { words[i / N] &= ~(1ull << (i % N)); }
+    [[nodiscard]] constexpr bool test  (size_t i)          const noexcept { return (words[i / 64] & (1ull << (i & 63))) > 0; }
+                  constexpr void set   (size_t i, bool v = true) noexcept { words[i / 64] |= (uint64_t(v) << (i & 63)); }
+                  constexpr void reset (size_t i)                noexcept { words[i / 64] &= ~(1ull << (i & 63)); }
                   constexpr void reset ()                        noexcept { for (auto& w: words) w = 0; }
 };
 
